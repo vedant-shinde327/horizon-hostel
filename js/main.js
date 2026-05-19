81ec7ac6073868658c961deeaebe63de9b854595
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-19 23:32:10 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+async function loadComponent(id, file) {
+    const response = await fetch(file);
+
+    const data = await response.text();
+
+    document.getElementById(id).innerHTML = data;
+}
+
+loadComponent("navbar", "./components/navbar.html")
