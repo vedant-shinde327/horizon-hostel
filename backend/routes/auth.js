78aee7179e26cd548174cb0f76715b84c4534bb3
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-23 15:29:25 +0530

    registration system integrated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,3 +3,40 @@ import bcrypt from "bcrypt";
 import User from "../models/user.js";
 
 const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    try {
+        const {
+            fullName,
+            mobileNo,
+            email,
+            password
+        } = req.body;
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser) {
+            return res.send("user already exist");
+        }
+
+        // hashed password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+
+        const newUser = new User({
+            fullName,
+            mobileNo,
+            email,
+            password:hashedPassword
+        });
+
+        await newUser.save();
+
+        res.send("Registration Successful");
+    } catch(err) {
+        console.log(err);
+        res.send("Registration Failed");
+    }
+});
+
+export default router;  
