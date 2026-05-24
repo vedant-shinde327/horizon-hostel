f996af56f38958deb9a25e6b48017fc38ae4e565
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-24 23:10:42 +0530

    sessions initialzed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -39,4 +39,41 @@ router.post("/register", async (req, res) => {
     }
 });
 
+router.post("/login", async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        // find User
+        const user = await User.findOne({email});
+
+        // user not found
+
+        if(!user) {
+            return res.send("User not found");
+        }
+
+        // compare pass
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if(!isMatch) {
+            return res.send("Invalid Credentials");
+        }
+        // 
+
+        req.session.user = {
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+        }
+        res.redirect("/dashboard.html");
+
+    } catch(err) {
+        console.log(err);
+        res.send("Login Failed");
+    }
+});
+
 export default router;  
