28293516750dbc897ec8449345bc8bb261e45ed0
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-24 23:12:08 +0530

    session preparing

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 app.get("/test-session", (req, res) => {
   res.send(req.session.user);
 });
-
+    
 app.listen(3000, () => {
     console.log("Server running");
 });
